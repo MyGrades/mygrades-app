92695477047aae978582d9aad817189148ad46bd
tilman <kontakt@tilman-ginzel.de>
2015-09-03 20:11:24 +0200

    Added rest endpoints

--- app/src/main/java/de/mobcomp/grades/main/rest/RestApi.java
+++ app/src/main/java/de/mobcomp/grades/main/rest/RestApi.java
@@ -1,8 +1,23 @@
 package de.mobcomp.grades.main.rest;
 
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+
 /**
  * Interface which defines all REST endpoints.
  */
 public interface RestApi {
-    // TODO: add endpoints
+
+    @GET("/universities")
+    Void getUniversities();
+
+    @GET("/universities/{university_id}")
+    Void getUniversity(@Path("university_id") int universityId);
+
+    @POST("/wishlist")
+    Void createWish();
+
+    @POST("/universities/{university_id}/errors")
+    Void createError(@Path("university_id") int universityId);
 }
