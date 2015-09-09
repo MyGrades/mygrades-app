d6def79345c9a437e2e8afdb1c4679784622f307
tilman <kontakt@tilman-ginzel.de>
2015-09-09 11:46:50 +0200

    Added BaseProcessor

---
+++ app/src/main/java/de/mygrades/main/processor/BaseProcessor.java
@@ -0,0 +1,24 @@
+package de.mygrades.main.processor;
+
+import android.content.ContentResolver;
+import android.content.Context;
+
+import de.mygrades.database.DBHelper;
+import de.mygrades.main.rest.RestClient;
+
+/**
+ * Base class for all processors to hold relevant objects.
+ */
+public class BaseProcessor {
+    private Context context;
+    private RestClient restClient;
+    private ContentResolver contentResolver;
+    private DBHelper dbHelper;
+
+    public BaseProcessor(Context context) {
+        this.context = context.getApplicationContext();
+        this.restClient = new RestClient(this.context);
+        this.contentResolver = this.context.getContentResolver();
+        this.dbHelper = new DBHelper(this.context);
+    }
+}
