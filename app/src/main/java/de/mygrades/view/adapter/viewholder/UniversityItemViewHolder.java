d49587479ec3d7d58c627ddada5a08eb1b583810
tilman <kontakt@tilman-ginzel.de>
2015-09-11 15:08:13 +0200

    Improved UI for university list view

--- app/src/main/java/de/mygrades/view/adapter/viewholder/UniversityItemViewHolder.java
+++ app/src/main/java/de/mygrades/view/adapter/viewholder/UniversityItemViewHolder.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import com.twotoasters.sectioncursoradapter.adapter.viewholder.ViewHolder;
 
+import de.mygrades.R;
+
 /**
  * ViewHolder for an university row.
  */
@@ -13,6 +15,6 @@ public class UniversityItemViewHolder extends ViewHolder {
 
     public UniversityItemViewHolder(View rootView) {
         super(rootView);
-        universityName = (TextView) rootView;
+        universityName = (TextView) rootView.findViewById(R.id.tv_university_name);
     }
 }
--- app/src/main/java/de/mygrades/view/adapter/viewholder/UniversitySectionViewHolder.java
+++ app/src/main/java/de/mygrades/view/adapter/viewholder/UniversitySectionViewHolder.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import com.twotoasters.sectioncursoradapter.adapter.viewholder.ViewHolder;
 
+import de.mygrades.R;
+
 /**
  * ViewHolder for the section title. (A, B, C...)
  */
@@ -13,6 +15,6 @@ public class UniversitySectionViewHolder extends ViewHolder {
 
     public UniversitySectionViewHolder(View rootView) {
         super(rootView);
-        sectionTitle = (TextView) rootView;
+        sectionTitle = (TextView) rootView.findViewById(R.id.tv_university_section);
     }
 }
