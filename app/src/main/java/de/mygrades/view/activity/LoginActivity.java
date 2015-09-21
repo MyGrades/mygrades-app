54247eaf1716245ea779234951d0ec73c492aac7
tilman <kontakt@tilman-ginzel.de>
2015-09-21 14:20:41 +0200

    Added input-check in LoginActivity and some minor improvements

--- app/src/main/java/de/mygrades/view/activity/LoginActivity.java
+++ app/src/main/java/de/mygrades/view/activity/LoginActivity.java
@@ -1,15 +1,23 @@
 package de.mygrades.view.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.securepreferences.SecurePreferences;
 
 import de.mygrades.R;
 import de.mygrades.main.MainServiceHelper;
+import de.mygrades.util.Constants;
 
 /**
  * Activity to enter the username and password for the selected university.
@@ -23,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     private long universityId;
 
     private TextView tvUniversityName;
+    private EditText etUsername;
+    private EditText etPassword;
     private Button btLogin;
 
     @Override
@@ -33,6 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         // set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.toolbar_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // get extra data
         Bundle extras = getIntent().getExtras();
@@ -43,18 +55,93 @@ public class LoginActivity extends AppCompatActivity {
         tvUniversityName = (TextView) findViewById(R.id.tv_university_name);
         tvUniversityName.setText(universityName);
 
-        // login button
-        btLogin = (Button) findViewById(R.id.bt_login);
-        btLogin.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(intent);
-            }
-        });
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_passwort);
+
+        initLoginButton();
 
         // load detailed university
         MainServiceHelper mainServiceHelper = new MainServiceHelper(this);
         mainServiceHelper.getDetailedUniversity(universityId);
+    }
+
+    /**
+     * Initialize login button.
+     */
+    private void initLoginButton() {
+        btLogin = (Button) findViewById(R.id.bt_login);
+        btLogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkInput()) {
+                    savePreferences();
+
+                    // TODO: save secure prefs asynchronous and wait for response before intent?
+                    saveLoginData();
+                    goToMainActivity();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // dirty hack to avoid recycler view bug
+        NavUtils.navigateUpFromSameTask(this);
+    }
+
+    /**
+     * Checks if username or password are empty and shows an error message.
+     *
+     * @return true if input is correct
+     */
+    private boolean checkInput() {
+        boolean inputCorrect = true;
+
+        if (TextUtils.isEmpty(etUsername.getText().toString())) {
+            etUsername.setError(getResources().getString(R.string.error_username_not_empty));
+            inputCorrect = false;
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            etPassword.setError(getResources().getString(R.string.error_password_not_empty));
+            inputCorrect = false;
+        }
+
+        return inputCorrect;
+    }
+
+    /**
+     * Saves universityId and logged-in state to preferences.
+     */
+    private void savePreferences() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong(Constants.PREF_KEY_UNIVERSITY_ID, universityId);
+        editor.putBoolean(Constants.PREF_KEY_LOGGED_IN, true);
+        editor.apply();
+    }
+
+    /**
+     * Saves username and password in secure preferences.
+     */
+    private void saveLoginData() {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        SharedPreferences prefs = new SecurePreferences(this, Constants.NOT_SO_SECURE_PREF_PW, Constants.NOT_SO_SECURE_PREF_FILE);
+        SecurePreferences.Editor editor = (SecurePreferences.Editor) prefs.edit();
+        editor.putString(Constants.PREF_KEY_USERNAME, username);
+        editor.putString(Constants.PREF_KEY_PASSWORD, password);
+        editor.apply();
+    }
+
+    /**
+     * Starts an intent to go to the main activity.
+     */
+    private void goToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        LoginActivity.this.startActivity(intent);
     }
 }
