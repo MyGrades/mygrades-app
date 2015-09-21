56ed3bdfb629e5ff8f7d596a4a5e6dbf41da0b23
tilman <kontakt@tilman-ginzel.de>
2015-09-21 22:31:25 +0200

    Changed MainActivity

--- app/src/main/java/de/mygrades/view/activity/MainActivity.java
+++ app/src/main/java/de/mygrades/view/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package de.mygrades.view.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +11,7 @@ import android.widget.Toast;
 
 import de.mygrades.R;
 import de.mygrades.main.MainServiceHelper;
+import de.mygrades.util.Constants;
 
 /**
  * Activity to show the overview of grades.
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!checkLogin()) {
+            goToUniversitySelection();
+            return;
+        }
+
         setContentView(R.layout.activity_main);
 
         btParse = (Button) findViewById(R.id.bt_parse);
@@ -28,5 +37,24 @@ public class MainActivity extends AppCompatActivity {
                 new MainServiceHelper(MainActivity.this).scrapeForGrades();
             }
         });
+    }
+
+    /**
+     * Checks if user is already logged in.
+     *
+     * @return true if user is logged in.
+     */
+    private boolean checkLogin() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        return prefs.getBoolean(Constants.PREF_KEY_LOGGED_IN, false);
+    }
+
+    /**
+     * Starts the SelectUniversityActivity.
+     */
+    private void goToUniversitySelection() {
+        Intent intent = new Intent(this, SelectUniversityActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
