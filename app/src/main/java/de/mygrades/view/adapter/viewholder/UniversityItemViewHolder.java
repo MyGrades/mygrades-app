76c3fdb4d9e9158911e566cecb667bbb0bb97225
tilman <kontakt@tilman-ginzel.de>
2015-09-22 19:00:38 +0200

    Implemented custom recycler view adapter

--- app/src/main/java/de/mygrades/view/adapter/viewholder/UniversityItemViewHolder.java
+++ app/src/main/java/de/mygrades/view/adapter/viewholder/UniversityItemViewHolder.java
@@ -2,6 +2,7 @@ package de.mygrades.view.adapter.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import de.mygrades.R;
 
@@ -11,8 +12,13 @@ import de.mygrades.R;
 public class UniversityItemViewHolder extends RecyclerView.ViewHolder {
     public final TextView tvUniversityName;
 
+    public final LinearLayout llSectionHeader;
+    public final TextView tvSectionHeader;
+
     public UniversityItemViewHolder(View rootView) {
         super(rootView);
         tvUniversityName = (TextView) rootView.findViewById(R.id.tv_university_name);
+        llSectionHeader = (LinearLayout) rootView.findViewById(R.id.ll_section_header);
+        tvSectionHeader = (TextView) rootView.findViewById(R.id.tv_section_title);
     }
 }
