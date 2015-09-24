3774c8070bcd7c885142061282276d337cba43f5
tilman <kontakt@tilman-ginzel.de>
2015-09-24 15:36:56 +0200

    Added method to dynamically add grades to the recycler view adapter

--- app/src/main/java/de/mygrades/view/adapter/GradesRecyclerViewAdapter.java
+++ app/src/main/java/de/mygrades/view/adapter/GradesRecyclerViewAdapter.java
@@ -29,9 +29,111 @@ public class GradesRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
         items = new ArrayList<>();
     }
 
-    public void add(GradesAdapterItem item, int position) {
-        items.add(position, item);
-        notifyItemInserted(position);
+    /**
+     * Add a new grade to the given semester by its termCount.
+     *
+     * @param newGrade - new grade
+     * @param termCount - semester term count
+     */
+    public void addGradeForSemester(GradeItem newGrade, int termCount) {
+        // find semester index, where the grade should be added
+        int semesterIndex = getIndexForSemester(termCount);
+
+        // find position in semester (lexicographic)
+        // start after the semesterIndex
+        int newGradeIndex = items.size();
+        for(int i = semesterIndex + 1; i < items.size(); i++) {
+            if (items.get(i) instanceof GradeItem) {
+                GradeItem gradeItem = (GradeItem) items.get(i);
+                if (newGrade.getName().compareToIgnoreCase(gradeItem.getName()) <= 0) {
+                    newGradeIndex = i;
+                    break;
+                }
+            } else if (items.get(i) instanceof SemesterItem) {
+                newGradeIndex = i;
+                break;
+            }
+        }
+
+        // add grade to semester and update semester header
+        SemesterItem semester = (SemesterItem) items.get(semesterIndex);
+        semester.addGrade(newGrade);
+        notifyItemChanged(semesterIndex);
+
+        // add grade to the item list
+        items.add(newGradeIndex, newGrade);
+        notifyItemInserted(newGradeIndex);
+    }
+
+    /**
+     * Get the index for a semester by its termCount.
+     *
+     * @param termCount - term count
+     * @return index
+     */
+    private int getIndexForSemester(int termCount) {
+        // find the index for a semester
+        int semesterIndex = -1;
+        for(int i = 0; i < items.size(); i++) {
+            if (items.get(i) instanceof SemesterItem) {
+                SemesterItem semesterItem = (SemesterItem) items.get(i);
+                if (semesterItem.getTermCount() == termCount) {
+                    semesterIndex = i;
+                    break;
+                }
+            }
+        }
+
+        // no semester found, add a new one
+        if (semesterIndex < 0) {
+            semesterIndex = addSemester(termCount);
+        }
+
+        return semesterIndex;
+    }
+
+    /**
+     * Adds a new semester to the item list.
+     * Semester are sorted descend by its term count (semester 3, semester 2, semester 1)
+     *
+     * @param termCount - term count
+     * @return index
+     */
+    private int addSemester(int termCount) {
+        int semesterIndex = -1; // -1, if there are no semesters so far
+
+        // find index where the semester should be added (descending by term count)
+        for(int i = 0; i < items.size(); i++) {
+            if (items.get(i) instanceof SemesterItem) {
+                SemesterItem semesterItem = (SemesterItem) items.get(i);
+                if (termCount > semesterItem.getTermCount()) {
+                    semesterIndex = i;
+                    break;
+                }
+            }
+        }
+
+        // if list is empty, use index 0
+        semesterIndex = items.size() == 0 ? 0 : semesterIndex;
+
+        // if new semester goes to bottom, use item.size()
+        semesterIndex = semesterIndex < 0 ? items.size() : semesterIndex;
+
+        // add new semester
+        SemesterItem newSemester = new SemesterItem();
+        newSemester.setTermCount(termCount);
+        items.add(semesterIndex, newSemester);
+        notifyItemInserted(semesterIndex);
+
+        return semesterIndex;
+    }
+
+    /**
+     * Clears all items.
+     */
+    public void clear() {
+        notifyItemRangeRemoved(0, items.size());
+        items.clear();
     }
 
     @Override
@@ -54,15 +156,15 @@ public class GradesRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
 
             viewHolder.tvTermCount.setText("" + semesterItem.getTermCount() + ". Semester");
             viewHolder.tvTermAsString.setText(semesterItem.getTermAsString());
-            viewHolder.tvAverage.setText("Ø " + semesterItem.getAverage());
-            viewHolder.tvCreditPoints.setText("Σ " + semesterItem.getCreditPoints() + " CP");
+            viewHolder.tvAverage.setText("Ø " + String.format("%.2f", semesterItem.getAverage()));
+            viewHolder.tvCreditPoints.setText("Σ " + String.format("%.1f", semesterItem.getCreditPoints()) + " CP");
         } else if (holder instanceof GradeViewHolder) {
             GradeViewHolder viewHolder = (GradeViewHolder) holder;
             GradeItem gradeItem = (GradeItem) items.get(position);
 
             viewHolder.tvName.setText(gradeItem.getName());
-            viewHolder.tvGrade.setText("" + gradeItem.getGrade());
-            viewHolder.tvCreditPoints.setText("" + gradeItem.getCreditPoints() + " CP");
+            viewHolder.tvGrade.setText("" + String.format("%.1f", gradeItem.getGrade()));
+            viewHolder.tvCreditPoints.setText("" + String.format("%.1f", gradeItem.getCreditPoints()) + " CP");
         }
     }
 
