b4ea4386d121eaf4d3b19b74f069617f2a2f377f
tilman <kontakt@tilman-ginzel.de>
2015-09-24 08:55:02 +0200

    Implemented recycler view adapter for overview of grades

---
+++ app/src/main/java/de/mygrades/view/model/GradeItem.java
@@ -0,0 +1,42 @@
+package de.mygrades.view.model;
+
+/**
+ * GradeItem is used in GradesRecyclerViewAdapter.
+ */
+public class GradeItem implements GradesAdapterItem {
+    private String name;
+    private float grade;
+    private float creditPoints;
+
+    public GradeItem() {}
+
+    public GradeItem(String name, float grade, float creditPoints) {
+        this.name = name;
+        this.grade = grade;
+        this.creditPoints = creditPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public float getCreditPoints() {
+        return creditPoints;
+    }
+
+    public void setCreditPoints(float creditPoints) {
+        this.creditPoints = creditPoints;
+    }
+}
