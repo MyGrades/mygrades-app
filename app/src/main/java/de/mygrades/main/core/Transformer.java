dc34184d529910f46c2a68ee624052d7c3183a4b
Jonas <asd>
2015-09-26 00:02:30 +0200

    comments on transformer

--- app/src/main/java/de/mygrades/main/core/Transformer.java
+++ app/src/main/java/de/mygrades/main/core/Transformer.java
@@ -36,12 +36,12 @@ public class Transformer {
     private Parser parser;
 
     /**
-     *
+     * HTML from which the data gets extracted.
      */
     private String html;
 
     /**
-     *
+     * Map of String -> TransformerMapping for easy access.
      */
     private Map<String, TransformerMapping> transformerMapping;
 
@@ -51,10 +51,16 @@ public class Transformer {
         this.html = html;
     }
 
-
+    /**
+     * Creates GradeEntry objects for all matching elements from html via
+     * xPath expression 'iterator' from TransformerMapping.
+     *
+     * @return List of extracted GradeEntries
+     * @throws ParseException if something goes wrong at parsing
+     */
     public List<GradeEntry> transform() throws ParseException {
         List<GradeEntry> gradeEntries = new ArrayList<>();
-        // get List to iterate through and respectively extract grade values
+        // get List to iterate through and respectively extract GradeEntry values
         NodeList nodeList = parser.parseToNodeList(transformerMapping.get(ITERATOR).getParseExpression(), html);
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node nNode = nodeList.item(i);
@@ -77,12 +83,28 @@ public class Transformer {
         return gradeEntries;
     }
 
+    /**
+     * Gets the value from Document determined by type of TransformerMapping as String.
+     *
+     * @param xmlDocument Document which should get parsed
+     * @param type Type of TransformerMapping regarding to GradeEntry
+     * @return extracted value as String
+     * @throws ParseException if something goes wrong at parsing
+     */
     private String getStringProperty(Document xmlDocument, String type) throws ParseException {
         String result = parser.parseToString(transformerMapping.get(type).getParseExpression(), xmlDocument).trim();
 
         return result;
     }
 
+    /**
+     * Gets the value from Document determined by type of TransformerMapping as Double.
+     *
+     * @param xmlDocument Document which should get parsed
+     * @param type Type of TransformerMapping regarding to GradeEntry
+     * @return extracted value as Double
+     * @throws ParseException if something goes wrong at parsing
+     */
     private Double getDoubleProperty(Document xmlDocument, String type) throws ParseException {
         String result = parser.parseToString(transformerMapping.get(type).getParseExpression(), xmlDocument).trim();
         result = result.replace(',', '.');
@@ -93,7 +115,12 @@ public class Transformer {
         return Double.parseDouble(result);
     }
 
-
+    /**
+     * Creates HashMap for TransformerMappings for easy access.
+     *
+     * @param transformerMappings which are put into Map
+     * @return Map of TransformerMappings
+     */
     private Map<String, TransformerMapping> createTransformerMappingMap(List<TransformerMapping> transformerMappings) {
         Map<String, TransformerMapping> map = new HashMap<>();
         for (TransformerMapping transformerMapping : transformerMappings) {
