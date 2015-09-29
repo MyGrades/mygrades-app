cabac916a22961cec75908423f60388d9fc76d18
tilman <kontakt@tilman-ginzel.de>
2015-09-29 14:07:04 +0200

    Removed PORT from ConfigDefault

--- app/src/main/java/de/mygrades/util/ConfigDefault.java
+++ app/src/main/java/de/mygrades/util/ConfigDefault.java
@@ -10,14 +10,11 @@ import android.util.Base64;
 public class ConfigDefault {
     // server info
     private static final String DOMAIN = "https://mygrades.de";
-    private static final int PORT = 443;
     private static final int API_VERSION = 1;
-    public static final String SERVER_URL = DOMAIN + ":" + PORT + "/api/v" + API_VERSION;
+    public static final String SERVER_URL = DOMAIN + "/api/v" + API_VERSION;
     public static final String API_USER = "MyGradesApi";
     public static final String API_PW = "AxPMiBxtgZXuGF28fVme";
     public static final String API_BASE64_CREDENTIALS = Base64.encodeToString((API_USER + ":" + API_PW).getBytes(), Base64.NO_WRAP);
-
-
 
     // user agent used in jsoup
     public static final String BROWSER_USER_AGENT = "Mozilla/5.0 (Linux; U; Android 2.2; en-gb; Nexus One Build/FRF50) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
