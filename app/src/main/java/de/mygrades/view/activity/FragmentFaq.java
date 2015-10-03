525763749c731b473fa8136aca7397bb2fa552c5
tilman <kontakt@tilman-ginzel.de>
2015-10-03 13:09:32 +0200

    Implemented NavigationDrawer with Fragments to switch content

---
+++ app/src/main/java/de/mygrades/view/activity/FragmentFaq.java
@@ -0,0 +1,24 @@
+package de.mygrades.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.mygrades.R;
+
+/**
+ * Created by tilman on 03.10.15.
+ */
+public class FragmentFaq extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+
+        return view;
+    }
+}
