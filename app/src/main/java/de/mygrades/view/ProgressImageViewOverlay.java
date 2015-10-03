af6c350982b6a3ea02660bbcf9f8a3bea26b0d7d
tilman <kontakt@tilman-ginzel.de>
2015-10-03 21:22:14 +0200

    Code cleanup

--- app/src/main/java/de/mygrades/view/ProgressImageViewOverlay.java
+++ app/src/main/java/de/mygrades/view/ProgressImageViewOverlay.java
@@ -2,13 +2,11 @@ package de.mygrades.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 /**
- * Created by tilman on 03.10.15.
+ * Custom ImageView which clips the image by a given progress.
  */
 public class ProgressImageViewOverlay extends ImageView {
     private float progress;
@@ -25,18 +23,11 @@ public class ProgressImageViewOverlay extends ImageView {
         super(context, attrs, defStyleAttr);
     }
 
-
-
     @Override
     public void onDraw(Canvas canvas) {
-        int left = getLeft();
-        int right = getRight();
-        int top = (int) (getTop() + getBottom() * (1f - progress));
-        int bottom = (int) (getBottom() * 1f);
+        int top = (int) (getBottom() * (1f - progress));
 
-        Log.v(".....", "draw " + progress);
-
-        canvas.clipRect(left, top, right, bottom);
+        canvas.clipRect(getLeft(), top, getRight(), getBottom());
         super.onDraw(canvas);
     }
 
@@ -46,6 +37,6 @@ public class ProgressImageViewOverlay extends ImageView {
 
     public void setProgress(float progress) {
         this.progress = progress;
-        invalidate();
+        invalidate(); // force re draw
     }
 }
