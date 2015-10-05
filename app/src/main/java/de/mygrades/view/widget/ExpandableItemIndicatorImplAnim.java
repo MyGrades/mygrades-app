b05e5a2f8e666cf330e5c43e4eabaf0c455d302c
tilman <kontakt@tilman-ginzel.de>
2015-10-05 16:22:20 +0200

    Added required resources for expandable recycler views

---
+++ app/src/main/java/de/mygrades/view/widget/ExpandableItemIndicatorImplAnim.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright (C) 2015 Haruki Hasegawa
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.mygrades.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.wnafee.vector.MorphButton;
+
+import de.mygrades.R;
+
+class ExpandableItemIndicatorImplAnim extends ExpandableItemIndicator.Impl {
+    private MorphButton mMorphButton;
+
+    @Override
+    public void onInit(Context context, AttributeSet attrs, int defStyleAttr, ExpandableItemIndicator thiz) {
+        View v = LayoutInflater.from(context).inflate(R.layout.widget_expandable_item_indicator_anim, thiz, true);
+        mMorphButton = (MorphButton) v.findViewById(R.id.morph_button);
+    }
+
+    @Override
+    public void setExpandedState(boolean isExpanded, boolean animate) {
+        MorphButton.MorphState indicatorState = (isExpanded) ? MorphButton.MorphState.START : MorphButton.MorphState.END;
+
+        if (mMorphButton.getState() != indicatorState) {
+            mMorphButton.setState(indicatorState, animate);
+        }
+    }
+}
