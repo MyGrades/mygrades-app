558266b13c8ea67024d9d9802e53bb1c08d459bd
tilman <kontakt@tilman-ginzel.de>
2015-10-13 13:26:11 +0200

    Added custom TypeAdapter to convert JSON boolean values

--- app/src/main/java/de/mygrades/main/rest/RestClient.java
+++ app/src/main/java/de/mygrades/main/rest/RestClient.java
@@ -1,10 +1,17 @@
 package de.mygrades.main.rest;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
 
 import de.mygrades.util.Config;
 import retrofit.RequestInterceptor;
@@ -15,6 +22,8 @@ import retrofit.converter.GsonConverter;
  * RestClient encapsulates all REST interfaces from Retrofit.
  */
 public class RestClient {
+    private static final String TAG = RestClient.class.getSimpleName();
+
     private Context context;
     private RestApi restApi;
 
@@ -31,6 +40,8 @@ public class RestClient {
         // initialize GSON converter
         Gson gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .registerTypeAdapter(Boolean.class, booleanAsIntAdapter)
+                .registerTypeAdapter(boolean.class, booleanAsIntAdapter)
                 .create();
 
         // initialize RestAdapter
@@ -48,4 +59,40 @@ public class RestClient {
     public RestApi getRestApi() {
         return restApi;
     }
+
+    /**
+     * Custom TypeAdapter to convert values from JSON to boolean.
+     *
+     * "1", true, 1 => true
+     * everything else => false
+     */
+    private static final TypeAdapter<Boolean> booleanAsIntAdapter = new TypeAdapter<Boolean>() {
+        @Override
+        public void write(JsonWriter out, Boolean value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+            } else {
+                out.value(value);
+            }
+        }
+
+        @Override
+        public Boolean read(JsonReader in) throws IOException {
+            JsonToken peek = in.peek();
+            switch (peek) {
+                case BOOLEAN:
+                    return in.nextBoolean();
+                case NULL:
+                    in.nextNull();
+                    return null;
+                case NUMBER:
+                    return in.nextInt() == 1;
+                case STRING:
+                    return in.nextString().equalsIgnoreCase("1");
+                default:
+                    Log.d(TAG, "Expected BOOLEAN, NUMBER or STRING but was " + peek);
+                    return false;
+            }
+        }
+    };
 }
