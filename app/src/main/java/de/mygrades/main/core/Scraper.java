251f215c62e0d76b6638fbd80862c7b081607963
jonastheis <mail@jonastheis.de>
2015-10-31 20:00:33 +0100

    removed unused import.

--- app/src/main/java/de/mygrades/main/core/Scraper.java
+++ app/src/main/java/de/mygrades/main/core/Scraper.java
@@ -10,7 +10,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Entities;
-import org.w3c.dom.NodeList;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
