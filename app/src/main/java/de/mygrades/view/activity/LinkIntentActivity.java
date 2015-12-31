e8d5de559810ad0fbb6a384f30537ba1d3158eba
jonastheis <mail@jonastheis.de>
2015-12-31 11:35:36 +0100

    added privacy policy fragment and comments to LinkIntentActivity.

--- app/src/main/java/de/mygrades/view/activity/LinkIntentActivity.java
+++ app/src/main/java/de/mygrades/view/activity/LinkIntentActivity.java
@@ -16,10 +16,14 @@ import de.mygrades.R;
 
 /**
  * Listens to an intent-filter and shows different fragments.
+ * Intent example: de.mygrades.view.activity://goto.fragment/faq/2
  */
 public class LinkIntentActivity extends AppCompatActivity implements ReplacableFragment {
     private static final String TAG = LinkIntentActivity.class.getSimpleName();
 
+    private static final String FAQ_PATH_SEGMENT = "faq";
+    private static final String REPORT_ERROR_PATH_SEGMENT = "reporterror";
+    private static final String PRIVACY_PATH_SEGMENT = "privacy";
 
     private Toolbar toolbar;
 
@@ -51,7 +55,7 @@ public class LinkIntentActivity extends AppCompatActivity implements ReplacableF
 
     /**
      * Evaluates which fragment should be shown and the options for that specific fragment.
-     * @param intentData Uri of the intent
+     * @param intentData Uri of the intent (e.g.:de.mygrades.view.activity://goto.fragment/faq/2)
      */
     private void evaluateIntentFragment(Uri intentData) {
         Log.d(TAG, intentData.toString());
@@ -59,7 +63,10 @@ public class LinkIntentActivity extends AppCompatActivity implements ReplacableF
         // get path segments and evaluate fragment
         List<String> pathSegments = intentData.getPathSegments();
         if (pathSegments.size() > 0) {
-            if ("faq".equals(pathSegments.get(0))) {
+
+            // get first path segment e.g. de.mygrades.view.activity://goto.fragment/faq/2 -> "faq"
+            String firstPathSegment = pathSegments.get(0);
+            if (FAQ_PATH_SEGMENT.equals(firstPathSegment)) {
                 // if a specific question for faq is set -> otherwise Fallback to faq
                 if (pathSegments.size() > 1) {
                     int goToQuestion = Integer.parseInt(pathSegments.get(1));
@@ -72,8 +79,11 @@ public class LinkIntentActivity extends AppCompatActivity implements ReplacableF
                     replaceFragment(R.id.fl_content, fragmentFaq, false);
                     return;
                 }
-            } else if ("reporterror".equals(pathSegments.get(0))) {
+            } else if (REPORT_ERROR_PATH_SEGMENT.equals(firstPathSegment)) {
                 replaceFragment(R.id.fl_content, new FragmentReportError(), false);
+                return;
+            } else if (PRIVACY_PATH_SEGMENT.equals(firstPathSegment)) {
+                replaceFragment(R.id.fl_content, new FragmentPrivacyPolicy(), false);
                 return;
             }
         }
