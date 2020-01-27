b9d037d3ebc682c9d18962b846492800716af6d2
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-01-28 00:35:59 +0100

    activate sentry

--- app/src/main/java/dh/mygrades/MyGradesApplication.java
+++ app/src/main/java/dh/mygrades/MyGradesApplication.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.preference.PreferenceManager;
 
 import dh.mygrades.R;
+import io.sentry.Sentry;
+import io.sentry.android.AndroidSentryClientFactory;
 import dh.mygrades.database.DatabaseHelper;
 import dh.mygrades.database.dao.DaoMaster;
 import dh.mygrades.database.dao.DaoSession;
@@ -19,6 +21,12 @@ public class MyGradesApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Init Sentry Bug Tracking
+        if (!BuildConfig.DEBUG) {
+            Sentry.init("https://9d668d239d8c435182b234391c0a1ce6@sentry.io/2014696?release=" + BuildConfig.VERSION_CODE, new AndroidSentryClientFactory(this));
+        }
+
 
         // set preferences default values
         PreferenceManager.setDefaultValues(this, R.xml.settings, false);
